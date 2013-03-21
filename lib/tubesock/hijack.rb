004baa7efe08ee9c516d30e7917d19e5a1b3f13c
Nick Gauthier <ngauthier@gmail.com>
2013-03-21 12:59:13 -0400

    clear AR connections when socket closes

--- lib/tubesock/hijack.rb
+++ lib/tubesock/hijack.rb
@@ -6,9 +6,12 @@ class Tubesock
     included do
       def hijack
         if Tubesock.websocket?(env)
-          rocket = Tubesock.hijack(env)
-          yield rocket
-          rocket.listen
+          sock = Tubesock.hijack(env)
+          yield sock
+          sock.onclose do
+            ActiveRecord::Base.clear_active_connections!
+          end
+          sock.listen
           render text: nil, status: -1
         else
           render text: "Not found", status: :not_found
