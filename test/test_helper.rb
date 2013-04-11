2a7a6c1ec5bef2e093ce4f379d31c47d3feb21f5
Nick Gauthier <ngauthier@gmail.com>
2013-04-11 15:31:00 -0400

    refactored test interaction

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,63 @@ require 'simplecov'
 require 'webrick'
 SimpleCov.start
 require 'tubesock'
+
+
+class Tubesock::TestInteraction
+  def initialize
+    @client_socket, @server_socket = UNIXSocket.pair
+  end
+
+  def handshake
+    @handshake ||= WebSocket::Handshake::Client.new(:url => 'ws://example.com')
+  end
+
+  def version
+    handshake.version
+  end
+
+  def env
+    request = WEBrick::HTTPRequest.new( :ServerSoftware => "" )
+    request.parse(StringIO.new(handshake.to_s))
+    rest = handshake.to_s.slice((request.to_s.length..-1))
+
+    request.meta_vars.merge(
+      "rack.input" => StringIO.new(rest),
+      "rack.hijack" => proc {},
+      "rack.hijack_io" => @server_socket
+    )
+  end
+
+  def write(message)
+    @client_socket << WebSocket::Frame::Outgoing::Client.new(
+      version: version,
+      data:    JSON.dump(message),
+      type:    :text
+    ).to_s
+  end
+
+  def read
+    framebuffer = WebSocket::Frame::Incoming::Client.new(version: version)
+    data, addrinfo = @client_socket.recvfrom(2000)
+    framebuffer << data
+    JSON.load(framebuffer.next.data)
+  end
+
+  def close
+    @client_socket.close
+  end
+
+  def tubesock
+    tubesock = Tubesock.hijack(env)
+    tubesock.listen
+    @client_socket.recvfrom 2000 # flush the handshake
+    tubesock
+  end
+end
+
+def Tubesock::TestPair
+  client_io, server_io = UNIXSocket.pair
+  client = Tubesock::TestClient.new(client_io)
+  server = Tubesock::TestServer.new(server_io)
+  [client, server]
+end
--- test/tubesock_test.rb
+++ test/tubesock_test.rb
@@ -7,23 +7,10 @@ describe Tubesock do
     }.must_raise Tubesock::HijackNotAvailable
   end
 
-  it "can hijack rack and send frames" do
-    client_io, server_io = UNIXSocket.pair
+  it "can hijack rack and send frames simply" do
+    interaction = Tubesock::TestInteraction.new
 
-    client = WebSocket::Handshake::Client.new(:url => 'ws://example.com')
-
-    request = WEBrick::HTTPRequest.new( :ServerSoftware => "" )
-    request.parse(StringIO.new(client.to_s)).must_equal true
-    rest    = client.to_s.slice((request.to_s.length..-1))
-
-    sock = Tubesock.hijack(request.meta_vars.merge(
-      "rack.input" => StringIO.new(rest),
-      "rack.hijack" => proc {},
-      "rack.hijack_io" => server_io
-    ))
-    sock.listen
-    # read the handshake to clear the socket
-    client_io.recvfrom(2000)
+    sock = interaction.tubesock
 
     opened = false
     closed = false
@@ -40,25 +27,13 @@ describe Tubesock do
       sock.send_data(message: "Hello #{message["name"]}")
     end
 
-    frame = WebSocket::Frame::Outgoing::Client.new(
-      version: client.version,
-      data:    JSON.dump(name: "Nick"),
-      type:    :text
-    )
-
-    client_io << frame.to_s
+    interaction.write name: "Nick"
 
     t = Thread.new do
       Thread.current.abort_on_exception = true
-      framebuffer = WebSocket::Frame::Incoming::Client.new(version: client.version)
-      data, addrinfo = client_io.recvfrom(2000)
-      framebuffer << data
-
-      frame = framebuffer.next
-
-      frame.wont_be_nil
-      JSON.load(frame.data)["message"].must_equal "Hello Nick"
-      client_io.close
+      data = interaction.read
+      data["message"].must_equal "Hello Nick"
+      interaction.close
     end
 
     t.join
