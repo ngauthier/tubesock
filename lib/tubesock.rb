39f48c244ba7c0a6c8aeadfb18c7eca59c663624
alex <alex@myservicefellow.com>
2014-11-12 14:32:37 +0100

    call readpartial and peeraddr on @socket if available to support puma's ssl sockets

--- lib/tubesock.rb
+++ lib/tubesock.rb
@@ -94,7 +94,12 @@ class Tubesock
   def each_frame
     framebuffer = WebSocket::Frame::Incoming::Server.new(version: @version)
     while IO.select([@socket])
-      data, addrinfo = @socket.recvfrom(2000)
+      if(@socket.respond_to?(:readpartial) && @socket.respond_to?(:peeraddr))
+        data, addrinfo = @socket.readpartial(2000), @socket.peeraddr
+      else
+        data, addrinfo = @socket.recvfrom(2000)
+      end
+
       break if data.empty?
       framebuffer << data
       while frame = framebuffer.next
@@ -106,6 +111,7 @@ class Tubesock
         end
       end
     end
+
   rescue Errno::EHOSTUNREACH, Errno::ETIMEDOUT, Errno::ECONNRESET, IOError, Errno::EBADF
     nil # client disconnected or timed out
   end
